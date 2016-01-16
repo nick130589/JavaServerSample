5875488ad8e71efe02d2f6b4a81262bfb3ed325d
alexromanov <al8x.romanov@gmail.com>
2016-01-16 11:22:31 +0300

    Added sysout on starting

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,7 +3,6 @@ package main;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.AllRequestsServlet;
 import servlets.MirrorServlet;
 
 /**
@@ -21,6 +20,7 @@ public class Main {
         server.setHandler(context);
 
         server.start();
+        System.out.println("Server started");
         server.join();
     }
 }
